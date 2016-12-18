a934e78ddc596013cd1a0e9c495bbcf2ba5933b5
Milos Zivlak <zivlakmilos@gmail.com>
2016-12-18 23:01:42 +0100

    Error constants

---
+++ error.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+SUCCESS                         =   0
+ERROR                           =  -1
+
+ERROR_WRONG_PARAMETERS          =  -2
+ERROR_MEMORY_ALLOCATION         =  -3
+
+ERROR_OPEN_PORT                 = -51
+ERROR_CLOSE_PORT                = -52
+ERROR_GET_PORT_OPTIONS          = -53
+ERROR_SET_PORT_OPTIONS          = -54
+
+ERROR_SET_DATA_BITS             = -61
+ERROR_SET_STOP_BITS             = -62
+ERROR_SET_PARITY                = -63
+ERROR_SET_BAUD_RATE             = -64
+
+ERROR_SENDING_DATA              = -71
+ERROR_RECIVEING_DATA            = -72
+
+ERROR_FREE                      = -0x50
+ERROR_ILLEGAL_VALUE             = -0x52
+ERROR_ILLEGAL_FORMAT            = -0x54
+ERROR_RUN_CHECKSUM              = -0x55
+ERROR_RUN_ID                    = -0x56
+ERROR_RUN_SYNTAX                = -0x57
+ERROR_RUN_NOT_SUPPORTED         = -0x58
+ERROR_ILLEGAL_ADDRESS           = -0x5A
+
