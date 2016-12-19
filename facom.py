a617cf501f959ea9b5bcc9e762465a91b000e5f6
Milos Zivlak <zivlakmilos@gmail.com>
2016-12-19 16:05:52 +0100

    Added some spaces (one line) after function doc

--- facom.py
+++ facom.py
@@ -9,6 +9,7 @@ class Facom:
 
     def __init__(self):
         'Construct object of type Facom, open library for communication'
+
         self.is_open = False
         try:
             self.facom = ctypes.CDLL('libfacom.so');
@@ -17,10 +18,12 @@ class Facom:
 
     def __del__(self):
         'Destructor of object of type Facom, close communication'
+
         self.close();
 
     def open(self, port, station_number = 0x01):
         'Open communication to Fatek PLC'
+
         error = self.facom.FACOM_open(port, station_number);
         if error < 0:
             self.is_open = False;
@@ -30,36 +33,44 @@ class Facom:
 
     def close(self):
         'Close communication with Fatek PLC'
+
         if self.is_open:
             return self.facom.FACOM_close();
         return ERROR;
 
     def set_data_bits(self, data_bits):
         'Set number of data bits'
+
         return self.facom.FACOM_setDataBits(data_bits);
 
     def set_parity(self, parity):
         'Set parity'
+
         return self.facom.FACOM_setParity(parity);
 
     def set_stop_bits(self, stop_bits):
         'Set number of stop bits'
+
         return self.facom.FACOM_setStopBits(parity);
 
     def set_baud_rate(self, baud_rate):
         'Set baud rate (speed)'
+
         return self.facom.FACOM_setBaudRate(baud_rate);
 
     def start(self):
         'Set PLC run mode to On'
+
         return self.facom.FACOM_start();
 
     def stop(self):
         'Set PLC run mode to Off'
+
         return self.facom.FACOM_stop();
 
     def set_discrete(self, discrete_type, discrete_number, action):
         'Set discrete'
+
         return self.facom.FACOM_setDiscrete(discrete_type, discrete_number, action);
 
     def set_discretes(self, discrete_type,
@@ -67,6 +78,7 @@ class Facom:
                             discrete_count,
                             data):
         'Set multiple continous discretes'
+
         return self.facom.FACOM_setDiscretes(discrete_type,
                                              discrete_number,
                                              discrete_count,
@@ -77,6 +89,7 @@ class Facom:
                             discrete_count,
                             data):
         'Get multiple continous discretes'
+
         return self.facom.FACOM_getDiscretes(discrete_type,
                                              discrete_number,
                                              discrete_count,
