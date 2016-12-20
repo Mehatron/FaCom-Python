d3fb2f182e9ecf8b7bc9b115bef284f83eea932d
Milos Zivlak <zivlakmilos@gmail.com>
2016-12-20 13:16:17 +0100

    Reorganize project struct and create install

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'FaCom',
+    version = '1.0',
+    description = 'Fatek communication protocole',
+    author = 'Milos Zivlak - ZI',
+    author_email = 'zivlakmilos@gmail.com',
+    url = 'https://github.com/Mehatron/FaCom-Python',
+    packages = ['facom/']
+)
+
